03a179809bc2cc53683e27b7e46bf5c088b80a40
Suraj Singh <suraj1252002@gmail.com>
2024-01-04 08:21:54 +0530

    This is a Day1 project which is a Bandname Generator Project

---
+++ 1st_day.py
@@ -0,0 +1,7 @@
+# Project 1 - Band Name Generator Project
+
+print("Welcome to band name generator 👇👇")
+city_name = input("What is your city name ? \n")
+pet_name = input("What's your pet name ? \n")
+band_name = city_name + " " + pet_name
+print("Your band name is : ", band_name)
